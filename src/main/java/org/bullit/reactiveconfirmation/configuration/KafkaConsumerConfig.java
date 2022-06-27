6b5759918ee1e687ae324801b301f2199904f2f7
Tim <tim@jtim.nl>
2022-06-27 17:05:18 +0200

    Improvement: * Use KafkaAdmin from Spring Kafka instead of the low level AdminClient.

--- src/main/java/org/bullit/reactiveconfirmation/configuration/KafkaConsumerConfig.java
+++ src/main/java/org/bullit/reactiveconfirmation/configuration/KafkaConsumerConfig.java
@@ -1,20 +1,20 @@
 package org.bullit.reactiveconfirmation.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.TopicPartitionInfo;
 import org.bullit.reactiveconfirmation.domain.Confirmation;
 import org.bullit.reactiveconfirmation.service.ConfirmationService;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.KafkaAdminClient;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.KafkaFuture;
-import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import reactor.core.publisher.Flux;
 import reactor.kafka.receiver.ReceiverOptions;
@@ -22,17 +22,21 @@ import reactor.kafka.receiver.ReceiverOptions;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
 @Slf4j
 @Configuration
 public class KafkaConsumerConfig {
+
+    private final KafkaAdmin kafkaAdmin;
+
+    public KafkaConsumerConfig(KafkaAdmin kafkaAdmin) {
+        this.kafkaAdmin = kafkaAdmin;
+    }
+
     @Bean
     public ReceiverOptions<String, Confirmation> kafkaReceiverOptions(KafkaProperties kafkaProperties,
                                                                       List<TopicPartition> partitionList) {
@@ -53,35 +57,10 @@ public class KafkaConsumerConfig {
 
     @Bean
     @DependsOn({"KafkaTopicConfig"})
-    public List<TopicPartition> topicPartitions(@Qualifier("topic") String topic,
-                                                @Qualifier("bootstrapAddress") String bootstrapAddress) throws InterruptedException, ExecutionException, TimeoutException {
-        Properties properties = new Properties();
-        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        AdminClient adminClient = KafkaAdminClient.create(properties);
-        return toCompletableFuture(
-                adminClient
-                        .describeTopics(Collections.singletonList(topic))
-                        .allTopicNames())
-                .thenApply(m -> m.get(topic).partitions())
-                .thenApply(pl -> pl
-                        .stream()
-                        .map(p -> new TopicPartition(topic, p.partition()))
-                        .collect(Collectors.toList())
-                )
-                .whenComplete((l, e) -> l.forEach(p -> log.info(String.format("Partition: %s", p))))
-                .get(3, TimeUnit.SECONDS);
-    }
-
-    private <T> CompletableFuture<T> toCompletableFuture(final KafkaFuture<T> kafkaFuture) {
-        final CompletableFuture<T> wrappingFuture = new CompletableFuture<>();
-        kafkaFuture.whenComplete((value, throwable) -> {
-            if (throwable != null) {
-                wrappingFuture.completeExceptionally(throwable);
-            } else {
-                wrappingFuture.complete(value);
-            }
-        });
-        return wrappingFuture;
+    public List<TopicPartition> topicPartitions(@Value("${kafka.topic}") String topic) {
+        Map<String, TopicDescription> topicMapping = kafkaAdmin.describeTopics(topic);
+        TopicDescription topicDescription = topicMapping.get(topic);
+        return toTopicPartitions(topic, topicDescription.partitions());
     }
 
     @Bean
@@ -103,5 +82,11 @@ public class KafkaConsumerConfig {
     public ConfirmationService confirmationService(Flux<Confirmation> confirmationFlux,
                                                    @Qualifier("timeout") Duration timeout) {
         return new ConfirmationService(confirmationFlux, timeout);
+    }
+
+    private List<TopicPartition> toTopicPartitions(String topic, List<TopicPartitionInfo> partitions) {
+        return partitions.stream()
+                .map(p -> new TopicPartition(topic, p.partition()))
+                .collect(Collectors.toList());
     }
 }
